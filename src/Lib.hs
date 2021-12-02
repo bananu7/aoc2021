3eeac8df92ce909a4ea70a05d8ea7d77e04deeb1
bananu7 <bananu7@o2.pl>
2021-12-02 10:10:01 +0100

    Refactored day 2

--- src/Lib.hs
+++ src/Lib.hs
@@ -22,40 +22,39 @@ main_2 =
     print
 
 -- day 2 part 1
-data Move = Up | Down | Forward
+data Move = Up Int | Down Int | Forward Int
 
-eval :: [(Move, Int)] -> (Int, Int)
-eval = foldl evalOne (0,0)
+eval :: [Move] -> Int
+eval = (\(x,y)->x*y) . foldl evalOne (0,0)
     where
-        evalOne (d, f) (Up, x) = (d-x, f)
-        evalOne (d, f) (Down, x) = (d+x, f)
-        evalOne (d, f) (Forward, x) = (d, f+x)
+        evalOne (d, f) (Up x) = (d-x, f)
+        evalOne (d, f) (Down x) = (d+x, f)
+        evalOne (d, f) (Forward x) = (d, f+x)
 
-parseLine :: String -> (Move, Int)
+parseLine :: String -> Move
 parseLine line =
     case words line of
-        [moveS, vS] -> (parseMove moveS, read vS)
+        [moveS, vS] -> parseMove moveS $ read vS
         _ -> error "wrong line format"
-
-parseMove :: String -> Move
-parseMove "up" = Up
-parseMove "down" = Down
-parseMove "forward" = Forward
+    where
+        parseMove "up" = Up
+        parseMove "down" = Down
+        parseMove "forward" = Forward
 
 main_3 =
     readFile "src/input_2.txt" >>=
-    return . (\(x,y)->x*y) . eval . map parseLine . lines >>=
+    return . eval . map parseLine . lines >>=
     print
 
 
 -- day 2 part 3
 
-evalAim :: [(Move, Int)] -> Int
+evalAim :: [Move] -> Int
 evalAim = (\(p,d,a) -> p*d) . foldl evalOne (0,0,0)
     where
-        evalOne (p, d, a) (Up, x)       = (p, d, a-x)
-        evalOne (p, d, a) (Down, x)     = (p, d, a+x)
-        evalOne (p, d, a) (Forward, x)  = (p+x, d+x*a, a)
+        evalOne (p, d, a) (Up x)       = (p, d, a-x)
+        evalOne (p, d, a) (Down x)     = (p, d, a+x)
+        evalOne (p, d, a) (Forward x)  = (p+x, d+x*a, a)
 
 main_4 =
     readFile "src/input_2.txt" >>=
