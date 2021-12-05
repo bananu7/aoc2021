d3c49ace3c0005a6dbd884123364af9f84100375
bananu7 <bananu7@o2.pl>
2021-12-05 10:05:40 +0100

    Cleanup

--- src/Day5.hs
+++ src/Day5.hs
@@ -31,33 +31,6 @@ parse s =
 
 between a (b,c) = a >= b && a <= c
 
-countOverlap :: Line -> Line -> Int
-countOverlap (HorizontalLine (x1, x2) y1) (HorizontalLine (x3, x4) y2) = 
-    if y1 /= y2 then 0 else rangeOverlap x1 x2 x3 x4
-
-countOverlap (VerticalLine x1 (y1, y2)) (VerticalLine x2 (y3, y4)) = 
-    if x1 /= x2 then 0 else rangeOverlap y1 y2 y3 y4
-
-countOverlap (VerticalLine x (y1, y2)) (HorizontalLine (x1, x2) y) = 
-    if x `between` (x1,x2) && y `between` (y1, y2) then 1 else 0
-
-countOverlap (HorizontalLine (x1, x2) y) (VerticalLine x (y1, y2)) = 
-    if x `between` (x1,x2) && y `between` (y1, y2) then 1 else 0
-
-countOverlap _ _ = 0
-
-rangeOverlap a b c d = 
-    if b >= c && b <= d then
-        if a < c then b-c+1 else b-a+1
-    else
-        if a >= c && a <= d then
-            d-a+1
-        else
-            if d <= b && c >= a then
-                d-c+1
-            else
-                0
-
 genOverlap :: Line -> Line -> [Point]
 genOverlap (HorizontalLine (x1, x2) y1) (HorizontalLine (x3, x4) y2) = 
     if y1 /= y2 then [] else genPointsHorizontal y1 x1 x2 x3 x4
@@ -76,12 +49,6 @@ genOverlap _ _ = []
 genPointsVertical x y1 y2 y3 y4 = [(x,y) | y <- [(min y1 y3)..(max y2 y4)], y >= y1 && y >= y3 && y <= y2 && y <= y4]
 genPointsHorizontal y x1 x2 x3 x4 = [(x,y) | x <- [(min x1 x3)..(max x2 x4)], x >= x1 && x >= x3 && x <= x2 && x <= x4]
 
-{-
-solve :: [Line] -> Int
-solve [] = 0
-solve (l:ls) = sum (map (countOverlap l) ls) + solve ls
--}
-
 solve :: [Line] -> Int
 solve ls = length . nub .  genAll $ ls
 
@@ -90,11 +57,9 @@ genAll [] = []
 genAll (l:ls) = (concat . map (genOverlap l) $ ls)++ genAll ls
 
 main_5_1 = do
-    input <- lines <$> readFile "src/input_5_test.txt"
+    input <- lines <$> readFile "src/input_5.txt"
     let ls = map parse input
     let answer = solve ls
-    --print ls
-    print . nub . genAll $ ls
     print answer
 
 main_5_2 :: IO ()
