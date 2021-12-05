07d0369082c7f7df082bd67ea0e9b97070814939
bananu7 <bananu7@o2.pl>
2021-12-05 11:48:14 +0100

    Added a much better implementation of Day5

---
+++ src/Day5alt.hs
@@ -0,0 +1,17 @@
+module Day5alt where
+
+import Day5 (Point, Line(..), parse, gen)
+
+import qualified Data.Map as M
+
+solve :: [Line] -> Int
+solve = length . filter (>= 2) . M.elems . foldl draw (M.fromList [])
+    where
+        draw :: M.Map Point Int -> Line -> M.Map Point Int
+        draw m l = M.unionWith (+) m (M.fromList $ zip (gen l) (repeat 1))
+
+main_5_1_alt = do
+    input <- lines <$> readFile "src/input_5.txt"
+    let ls = map parse input
+    let answer = solve ls
+    print answer
