337b1d731bc4e8259517ebba940dcb83a6e4f4b6
bananu7 <bananu7@o2.pl>
2021-12-05 10:53:58 +0100

    d5p2 working

--- src/Day5.hs
+++ src/Day5.hs
@@ -3,6 +3,7 @@ module Day5 where
 
 import Data.Char (isDigit)
 import Data.List (nub, intersect)
+import qualified Data.Set as S
 
 import Debug.Trace
 -- array                   :: (Ix a) => (a,a) -> [(a,b)] -> Array a b
@@ -22,7 +23,10 @@ parse s =
             else if x1 == x2
                 then VerticalLine x1 (min y1 y2, max y1 y2)
             else
-                DiagonalLine (min x1 x2, min y1 y2) (max x1 x2, max y1 y2)
+                if x2 >= x1 then -- go right
+                    DiagonalLine (x1, y1) (x2, y2)
+                else
+                    DiagonalLine (x2, y2) (x1, y1)
     where
         numbers :: [Int]
         numbers = map read . words . map symbolToSpace $ s
@@ -67,10 +71,22 @@ main_5_1 = do
 gen :: Line -> [Point]
 gen (HorizontalLine (x1, x2) y)         = [(x,y) | x <- [x1..x2]]
 gen (VerticalLine x (y1, y2))           = [(x,y) | y <- [y1..y2]]
-gen (DiagonalLine (x1, y1) (x2, y2))    = zip [x1..x2] [y1..y2]
+gen (DiagonalLine (x1, y1) (x2, y2))    = zip [x1..x2] (if y2 > y1 then [y1..y2] else [y1, y1-1 .. y2])
 
 genOverlap' :: Line -> Line -> [Point]
-genOverlap' a b = gen a `intersect` gen b
+genOverlap' (HorizontalLine (x1, x2) y1) (HorizontalLine (x3, x4) y2) = 
+    if y1 /= y2 then [] else genPointsHorizontal y1 x1 x2 x3 x4
+
+genOverlap' (VerticalLine x1 (y1, y2)) (VerticalLine x2 (y3, y4)) = 
+    if x1 /= x2 then [] else genPointsVertical x1 y1 y2 y3 y4
+
+genOverlap' (VerticalLine x (y1, y2)) (HorizontalLine (x1, x2) y) = 
+    if x `between` (x1,x2) && y `between` (y1, y2) then [(x, y)] else []
+
+genOverlap' (HorizontalLine (x1, x2) y) (VerticalLine x (y1, y2)) = 
+    if x `between` (x1,x2) && y `between` (y1, y2) then [(x, y)] else []
+
+genOverlap' a b = S.toList $ (S.fromList $ gen a) `S.intersection` (S.fromList $ gen b)
 
 solve' :: [Line] -> Int
 solve' ls = length . nub .  genAll' $ ls
@@ -83,5 +99,6 @@ main_5_2 = do
     input <- lines <$> readFile "src/input_5.txt"
     let ls = map parse input
     let answer = solve' ls
+    print . genAll' $ ls
     print answer
 
