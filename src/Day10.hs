95fda9a7bf7946ef15c765ba7800e0932d056289
bananu7 <bananu7@o2.pl>
2021-12-10 07:29:32 +0100

    day10p2

--- src/Day10.hs
+++ src/Day10.hs
@@ -1,27 +1,30 @@
 module Day10 where
 
 import Data.Maybe
+import Data.List
 
-parseLine :: String -> Maybe Char
+data Result = Clean | Broken Char | Incomplete String
+
+parseLine :: String -> Result
 parseLine = parseLine' []
 
-parseLine' ('<':a) ('>':b) = parseLine' a b
+parseLine' ('(':a) (')':b) = parseLine' a b
 parseLine' ('[':a) (']':b) = parseLine' a b
 parseLine' ('{':a) ('}':b) = parseLine' a b
-parseLine' ('(':a) (')':b) = parseLine' a b
+parseLine' ('<':a) ('>':b) = parseLine' a b
 
-parseLine' a ('<':b) = parseLine' ('<':a) b
+parseLine' a ('(':b) = parseLine' ('(':a) b
 parseLine' a ('[':b) = parseLine' ('[':a) b
 parseLine' a ('{':b) = parseLine' ('{':a) b
-parseLine' a ('(':b) = parseLine' ('(':a) b
+parseLine' a ('<':b) = parseLine' ('<':a) b
 
-parseLine' _ ('>':b) = Just '>'
-parseLine' _ (']':b) = Just ']'
-parseLine' _ ('}':b) = Just '}'
-parseLine' _ (')':b) = Just ')'
+parseLine' _ (')':b) = Broken ')'
+parseLine' _ (']':b) = Broken ']'
+parseLine' _ ('}':b) = Broken '}'
+parseLine' _ ('>':b) = Broken '>'
 
-parseLine [] [] = Nothing -- clean
-parseLine' _ [] = Nothing -- incomplete
+parseLine' [] [] = Clean -- clean
+parseLine' a [] = Incomplete a -- incomplete
 
 score :: Char -> Integer
 score ')' = 3
@@ -29,11 +32,39 @@ score ']' = 57
 score '}' = 1197
 score '>' = 25137
 
+catBroken [] = []
+catBroken (Broken a : r) = a : catBroken r
+catBroken (_:r) = catBroken r
+
 main_10_1 = do
     input <- lines <$> readFile "src/input_10.txt"
 
-    let s = sum . map score . catMaybes . map parseLine $ input
+    let s = sum . map score . catBroken . map parseLine $ input
 
     print s
 
+
 -- part2
+
+catIncomplete [] = []
+catIncomplete (Incomplete a : r) = a : catIncomplete r
+catIncomplete (_:r) = catIncomplete r
+
+val :: Char -> Int
+val '(' = 1
+val '[' = 2
+val '{' = 3
+val '<' = 4
+
+score2 :: String -> Int
+score2 = foldl one 0
+    where
+        one s c = s*5 + val c 
+
+main_10_2 = do
+    input <- lines <$> readFile "src/input_10.txt"
+
+    let scores = sort . map score2 . catIncomplete . map parseLine $ input
+    let s = head . drop (length scores `div` 2) $ scores
+
+    print s
